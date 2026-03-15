d0d3e955cb7a374a1303806889d4f80ef08c6421
Martin-Rouault <martin.rlt24@gmail.com>
2026-03-15 17:44:24 +0100

    add Thread model and controller

---
+++ app/Http/Controllers/API/ThreadController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Thread;
+use Illuminate\Http\Request;
+
+class ThreadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $threads = Thread::all();
+
+        return response()->json([
+            'threads' => $threads
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string'
+        ]);
+
+        $thread = Thread::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'user_id'=> $request->user()->id
+        ]);
+
+        return response()->json($thread, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ThreadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +12,11 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:60,1');
 
+Route::get('/thread', [ThreadController::class, 'index']);
+Route::get('/thread/{thread}', [ThreadController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/threads', [ThreadController::class, 'store'])->name('threads.create');
+    Route::put('thread/update', [ThreadController::class, 'update'])->name('threads.update');
 });
